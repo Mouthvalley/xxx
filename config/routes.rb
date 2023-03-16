49aed49027680054737e2ce8ecbab61c196313c3
Mouthvalley <45648573+Mouthvalley@users.noreply.github.com>
2023-03-16 06:51:51 +0000

    [3/16]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
 
-# 顧客用
-# URL /users/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: 'homes#top'
+    get "about" => "homes#about"
+  end
+
 end
