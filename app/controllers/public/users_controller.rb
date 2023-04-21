548984625a959a9866ba4ede2a2408b8bb0511f2
Mouthvalley <45648573+Mouthvalley@users.noreply.github.com>
2023-04-21 07:34:07 +0000

    Add

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,11 @@
 class Public::UsersController < ApplicationController
+
+  def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :password, :email, :is_deleted)
+  end
 end
